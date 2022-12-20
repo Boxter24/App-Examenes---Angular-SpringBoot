55f99d369df7fc0e07d0566951e6456ac57a7902
Boxter24 <jmquintero2210@gmail.com>
2022-12-20 11:24:19 -0400

    primer commit

---
+++ src/main/java/Boxter/AppExamenesAPI/AppExamenesApiApplication.java
@@ -0,0 +1,13 @@
+package Boxter.AppExamenesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExamenesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExamenesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Boxter/AppExamenesAPI/AppExamenesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Boxter.AppExamenesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExamenesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
